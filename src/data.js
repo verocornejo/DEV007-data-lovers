7b5f0a7b01672e4d7c2eeed8213e303b14a1f73b
Veronica <130104127+verocornejo@users.noreply.github.com>
2023-05-31 11:55:42 -0400

    inicio funcion ordenar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const sortName = () => {
+  pokemones.sort((a,b) => {
+  if(a.name.pokemon < b.name.pokemon) { 
+    return -1; } //si el nombre de a es menor que el de b, lo posiciona primero
+  if(a.name.pokemon > b.name.pokemon) { 
+    return 1; } //si el nombre de a es mayor que el de b, lo posiciona despues
+  return 0; // si el nombre de a es igual al de b, no los cambia de posicion
+  });
+  contenedor.innerHTML = "";
+  mostrarPokemones(pokemones);
+  };
+  //para llamar la funcion en el html Ordenar por nombre
